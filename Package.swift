285c1dc6f5f9996ccb86b7ae4c61ae66ee42c3c2
Mac <maclong9@icloud.com>
2026-02-01 12:26:12 +0100

    Use Web UI main branch dependency

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v15), .tvOS(.v13), .iOS(.v16), .watchOS(.v6), .visionOS(.v2),
     ],
     dependencies: [
-        .package(path: "../../tooling/web-ui"),
+        .package(url: "https://github.com/mac9sb/web-ui", branch: "main"),
         .package(url: "https://github.com/swiftlang/swift-testing", from: "0.11.0"),
         .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.0.0"),
     ],
